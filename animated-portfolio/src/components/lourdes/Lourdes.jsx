54cf6039501c24e72e053193210e6f926792b5f4
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-11-07 16:33:58 -0500

    wrote code for navabr to create hamburger menu

---
+++ animated-portfolio/src/components/lourdes/Lourdes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Lourdes.scss";
+import {motion} from "framer-motion";
+
+const Lourdes = () => {
+ return (
+    <div className="lourdes">
+      <div className="textContainer"></div>
+      Lourdes 
+
+    </div>
+  )
+}; 
+
+export default Lourdes;
--- animated-portfolio/src/components/navbar/navbar.jsx
+++ animated-portfolio/src/components/navbar/navbar.jsx
@@ -1,13 +1,21 @@
-import "./navbar.scss";  
+import Sidebar from "../sidebar/sidebar";
+import "./navbar.scss";
+import {motion} from "framer-motion"
 
 const Navbar = () => {
     return(
         <div className="navbar"> 
         {/* Sidebar */}
+        <Sidebar/>  
         <div className="wrapper">
-            <span>LoMiDo</span>
+            <motion.span 
+            initial={{opacity:0, scale: 0.5}}
+             animate={{opacity:1, scale: 1}}
+             transition={{duration:0.5}}
+             >
+             Lourdes Dorvilus</motion.span>
             <div className="social"></div>
-            <a href="#"><img src="/React_Portfolio/animated-portfolio/public/linkedin.png" alt="Logo of Linkedin" /></a>
+            <a href="#"><img src="/linkedin.png" alt="Logo of Linkedin" /></a>
             <a href="#"><img src="/GitHub-logo.png" alt="Logo of Git Hub" /></a>
         </div>
         </div>
